69304f70052b6241645bd5400928df2988643f79
tiennt <tiennt@vega.com.vn>
2015-12-14 17:19:17 +0700

    add quotev stories

--- quotev.js
+++ quotev.js
@@ -4,8 +4,8 @@ var slug            = require('slug');
 var async           = require('async');
 var configuration   = require('./configuration');
 
-var connection = mysql.createConnection(configuration.MYSQL_CONFIG);
 function listStory(link){
+    var connection = mysql.createConnection(configuration.MYSQL_CONFIG);
     var c = new Crawler({
         'maxConnections':10,
         'forceUTF8': true,
@@ -47,6 +47,68 @@ function listStory(link){
     }]);
 }
 
+
+function run(){
+    var connection = mysql.createConnection(configuration.MYSQL_CONFIG);
+
+    connection.connect(function(error){
+        if(error){
+            console.log('error connecting: ',error.stack);
+            return;
+        }
+        console.log('Connected mysql db');
+    });
+
+
+    //get status pending to crawler
+    var query = 'SELECT id,story_name, link from quotev_story WHERE status=1 order by id desc LIMIT 1';
+    connection.query(query, function(err, rows) {
+        // connected! (unless `err` is set)
+        if(err){
+            console.error("Get data error: ", err.stack);
+            return;
+        }
+        //console.log(rows);
+        if(rows.length==0){
+            console.log('Khong co du lieu nao!');
+            connection.end();
+            return;
+        }
+        async.each(rows, function(row,cb){
+            console.log(row);
+            var c = new Crawler({
+                'maxConnections':10,
+                'forceUTF8': true,
+                'userAgent': 'Mozilla/5.0 (X11; Linux i686 (x86_64)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36',
+                'callback':function(error,result,$){}
+            });
+
+            c.queue([{
+                'uri': row.link,
+                'callback': function (error, result, $) {
+                    var chapters = [];
+                    $('#rselect div a').each(function(i,a){
+                        var chapter = {
+                            story_id:row.id,
+                            story_name:row.story_name,
+                            chapter_number: (i + 1),
+                            chapter_name : $(a).text(),
+                            chapter_link : $(a).attr('href'),
+                        }
+                        chapters.push(chapter);
+                    });
+                    console.log(chapters);
+                }
+            }]);
+
+            return cb(null);
+        }, function(error){
+            console.log('FINISHED!!');//dong ket noi
+        });
+
+    });
+}
+
 function getDetail(info){
     var c = new Crawler({
         'maxConnections':10,
@@ -78,5 +140,12 @@ function getDetail(info){
         }
     }]);
 }
+/**
+ * lay ra danh sach truyen
+ */
+//listStory('http://www.quotev.com/stories/c/Romance?v=users&page=1');
 
-listStory('http://www.quotev.com/stories/c/Romance?v=users&page=1');
+/**
+ * lay ra cac chuong cua truyen
+ */
+run();
